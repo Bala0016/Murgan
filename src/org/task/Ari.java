2e91002ff2f3b1a896d0302fd1b817f91c570cb0
Bala0016 <balamurugan.velaa@gmail>
2022-03-14 10:10:42 +0530

    code added

---
+++ src/org/task/Ari.java
@@ -0,0 +1,20 @@
+package org.task;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Ari {public static void main(String[] args) {
+	List<Integer>li=new LinkedList<Integer>();
+	li.add(2);
+	li.add(1);
+	li.add(4);
+	li.add(3);
+	li.add(5);
+
+	for (int i = 0; i < li.size();i=i+2) {
+		
+		System.out.println(li.get(i));
+
+}
+}
+}
