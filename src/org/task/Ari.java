505d9d9b531c23bf1ea90d5946e4b2f6cc334565
Austinraj95 <austinraj94@gmail.com>
2022-03-16 12:29:09 +0530

    new

--- src/org/task/Ari.java
+++ src/org/task/Ari.java
@@ -14,9 +14,10 @@ public class Ari {public static void main(String[] args) {
 	for (int i = 0; i < li.size();i=i+2) {
 		
 		System.out.println(li.get(i));
-		System.out.println(li.get(i));
-		System.out.println(li.get(i));
+		System.out.println("conflict");
+		System.out.println("conflict resoled");
 
+        
 		
 }
 }
